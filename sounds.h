9cd4b9219837e98b12437b05f2a1bdc850d87982
Brandon F <93058771+bjf54@users.noreply.github.com>
2021-12-05 22:32:15 -0500

    Create sounds.h

---
+++ sounds.h
@@ -0,0 +1,16 @@
+#ifndef SOUNDS_H
+#define SOUNDS_H
+
+const uint16_t SOUND_START[] PROGMEM = {
+	100, 100, 200, 100, 300, 100, 400, 100, 500, 100, TONES_END
+};
+
+const uint16_t SOUND_GAME_OVER[] PROGMEM = {
+	500, 100, 400, 100, 300, 100, 200, 100, 100, 100, TONES_END
+};
+
+const uint16_t SOUND_OK[] PROGMEM = {
+	500, 100, 0, 20, 500, 100, TONES_END
+};
+
+#endif
